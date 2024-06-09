a9caf549bc8f2b5e89f892ecc46a2161f810479d
juhyun <zzang9ha@naver.com>
2024-06-09 13:26:45 +0900

    Problem14 - 신규 아이디 추천

---
+++ src/main/java/string/Problem14.java
@@ -0,0 +1,32 @@
+package string;
+
+/**
+ * 신규 아이디 추천 <p>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/72410
+ */
+public class Problem14 {
+
+    public String solution(String newId) {
+        newId = newId.toLowerCase();
+
+        newId = newId.replaceAll("[^a-z0-9\\-_.]", "");
+
+        newId = newId.replaceAll("\\.+", "");
+
+        newId = newId.replaceAll("^\\.+|\\.+$", "");
+
+        if (newId.isEmpty()) newId = "a";
+
+        if (newId.length() >= 16) {
+            newId = newId.substring(0, 15);
+            newId = newId.replaceAll("\\.$", "");
+        }
+
+        while (newId.length() < 3) {
+            newId += newId.charAt(newId.length() - 1);
+        }
+
+        return newId;
+    }
+
+}
